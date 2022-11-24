a82702faf2ddb8620b6100cd8d6d5e1cd9531212
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-24 20:51:31 +0530

    REFACTORED: Code to edit user details

--- src/com/company/AddressBook.java
+++ src/com/company/AddressBook.java
@@ -6,18 +6,50 @@ import java.util.stream.Collectors;
 
 public class AddressBook {
     private List<Contact> contactList;
+    private Integer editIndex;
 
     AddressBook() {
         contactList = new ArrayList<>();
     }
 
     public void addContact(Contact contact) {
+     if(isContactListed(contact)){
+         System.out.println("Contact Already Exists");
+         return;
+     }
+        contactList.add(contact);
+    }
+    public boolean checkIfContactExistsUsingName(String firstName,String lastName){
+        for(int i=0;i<contactList.size();i++){
+            if(contactList.get(i).getFirstName().equals(firstName)&&contactList.get(i).getLastName().equals(lastName)) {
+                editIndex=i;
+                return true;
+            }
+        }
+        return false;
+    }
+    public void editContactDetails(Contact contact) {
+      if(editIndex!=null){
+          contactList.set(editIndex,contact);
+          editIndex=null;
+      }
+
+    }
+
+    private boolean isContactListed(Contact contact) {
         for (Contact x : contactList) {
             if (x.equals(contact)) {
                 System.out.println("Contact Already Exists");
-                return;
+                return true;
             }
         }
-        contactList.add(contact);
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "AddressBook{" +
+                "contactList=" + contactList +
+                '}';
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,60 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+    private AddressBook addressBook = new AddressBook();
 
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
-        Contact contact = new Contact("Mazhar", "Ali", "Hyderabad", "500008", "8125629427", "syedmazharali742@gmail.com","syedmazharali742@gmail.com");
-        AddressBook addressBook = new AddressBook();
-        addressBook.addContact(contact);
+        Main main = new Main();
+        main.addContactToAddressBook();
+        main.printAddressBook();
+        main.editUserDetails();
+    }
 
+    private void editUserDetails() {
+        System.out.println("Please Enter First And Last Name to Edit Contact");
+        String firstName = scanner.next();
+        String lastName = scanner.next();
+        boolean flag = isContactEditable(firstName, lastName);
+        if (!flag) {
+            System.out.println("Contact Doesn't Exists With the given First Name and Last Name");
+            return;
+        }
+        updateUserDetailsToContactList();
+    }
+
+    private void updateUserDetailsToContactList() {
+        System.out.println("Please Enter first Name");
+        String firstName = scanner.next();
+        System.out.println("Please Enter last Name");
+        String lastName = scanner.next();
+        System.out.println("Please Enter city");
+        String city = scanner.next();
+        System.out.println("Please Enter State");
+        String state = scanner.next();
+        System.out.println("Please Enter zip code");
+        String zipCode = scanner.next();
+        System.out.println("Please Enter phoneNumber");
+        String phoneNumber = scanner.next();
+        System.out.println("Please Enter Email");
+        String email = scanner.next();
+        Contact c2 = new Contact(firstName, lastName, city, state, zipCode, phoneNumber, email);
+        addressBook.editContactDetails(c2);
+    }
+
+    private boolean isContactEditable(String firstName, String lastName) {
+        return addressBook.checkIfContactExistsUsingName(firstName, lastName);
+    }
+
+    private void printAddressBook() {
+        System.out.println(addressBook);
+    }
+
+    private void addContactToAddressBook() {
+        Contact contact = new Contact("Mazhar", "Ali", "Hyderabad", "500008", "8125629427", "syedmazharali742@gmail.com", "syedmazharali742@gmail.com");
+        addressBook.addContact(contact);
     }
 }
